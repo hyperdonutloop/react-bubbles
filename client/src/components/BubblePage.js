d1f87bbab6bdf514d1cafdd6af08dfecb9873bdd
donutwizard666 <thebeardedwarrior@icloud.com>
2019-11-15 08:58:40 -0800

    built out axios request and successfully rendered bubble data to the DOM

--- client/src/components/BubblePage.js
+++ client/src/components/BubblePage.js
@@ -3,11 +3,25 @@ import axios from "axios";
 
 import Bubbles from "./Bubbles";
 import ColorList from "./ColorList";
+import { axiosWithAuth } from "../utils/axiosWithAuth";
 
 const BubblePage = () => {
   const [colorList, setColorList] = useState([]);
-  // fetch your colors data from the server when the component mounts
-  // set that data to the colorList state property
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const getData = () => {
+    axiosWithAuth()
+      .get('/api/colors')
+      .then(response => {
+        setColorList(response.data);
+      })
+      .catch(error => {
+        console.log('the data was not returned', error);
+      })
+  };
 
   return (
     <>
